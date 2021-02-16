220233b84d2c04beaa076a0110515094aa0b497f
juandtr1999 <jr9282507@gmail.com>
2021-02-16 17:12:25 -0500

    my project c Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
- * reset_to_98 - function
- * @n: variable
- * Return: 0
+ * reset_to_98 - Change pointer content to 98.
+ * Description: Change pointer to int and replace content
+ * to 98.
+ * @n: pointer to change;
+ * Return: 0.
  */
 void reset_to_98(int *n)
 {
