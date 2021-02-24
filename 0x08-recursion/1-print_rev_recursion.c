531dda0437f664eb0c6f3e1fe3ea1dbedc5196e3
juandtr1999 <jr9282507@gmail.com>
2021-02-24 17:06:39 -0500

    C - Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints buffer in hexa
+ * @s: the address of memory to print
+ * Return: s.
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}#include "holberton.h"
+ /**
+  * _print_rev_recursion - prints buffer in hexa
+  * @s: the address of memory to print
+  * Return: s.
+  */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
