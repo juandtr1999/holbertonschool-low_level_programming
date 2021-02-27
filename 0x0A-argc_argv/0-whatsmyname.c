349145ca2f52a9a27120631bedc445cb218f19fd
juandtr1999 <jr9282507@gmail.com>
2021-02-27 13:27:41 -0500

    C -argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print file name
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+  printf("%s\n", argv[0]);
+
+  return (0);
+}
