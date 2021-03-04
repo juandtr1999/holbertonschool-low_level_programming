45f10e101a762547ad26a97843e5e240dbc7ae03
juandtr1999 <jr9282507@gmail.com>
2021-03-04 16:42:31 -0500

    project More malloc, free

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,6 @@
+int _putchar(char c);
+void *malloc_checked( int bsin firmar);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc( int nmembsin firmar, tamaño int sin firmar);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
