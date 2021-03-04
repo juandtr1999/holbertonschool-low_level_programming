18a323f260c07d04cb9d2b7ffbb248fb85fb786c
juandtr1999 <jr9282507@gmail.com>
2021-03-04 17:40:52 -0500

    project More malloc, free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,22 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
+#include "holberton.h"
 /**
- *malloc_checked - allocate memory using malloc
- *@b: unsigned int
- *Return: a pointer to allocated memory
+ * *malloc_checked - program that prints its name.
+ * @b: array of character pointers
+ * Return: 0.
  */
-
 void *malloc_checked(unsigned int b)
 {
   void *p;
 
   p = malloc(b);
+
   if (p == NULL)
     {
+      free(p);
+
       exit(98);
     }
   return (p);
