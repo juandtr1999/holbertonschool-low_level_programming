b6062ac0167bb0812607cafe062b3859f385ba48
juandtr1999 <jr9282507@gmail.com>
2021-03-04 18:16:32 -0500

    project more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,15 +6,14 @@
  *@b: unsigned int
  *Return: a pointer to allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-  void *p;
+	void *p;
 
-  p = malloc(b);
-  if (p == NULL)
-    {
-      exit(98);
-    }
-  return (p);
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
 }
