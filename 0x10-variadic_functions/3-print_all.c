e9982287c6b8c111b3b616b15d185175f250625a
juandtr1999 <jr9282507@gmail.com>
2021-03-11 19:40:09 -0500

    project variadic_functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * p_char - program that prints its name.
+
+ * @c: array of character pointers
+
+ * Return: arg:
+
+ */
+void p_char(va_list c)
+{
+
+	printf("%c", va_arg(c, int));
+}
+
+/**
+
+ * p_integer - program that prints its name.
+
+ * @i: array of character pointers
+
+ * Return: arg.
+
+ */
+
+void p_intege(va_list)
+{
+
+	char *stg;
+
+	stg = va_arg(s, char *);
+	if (stg == NULL)
+		stg = "(nil)";
+
+}
+
+/**
+
+ * print_all - program that prints its name.
+
+ * @format: array of character pointers
+
+ * Return: arg.
+
+ */
+
+void print_all(const char * const format, ...)
+
+{
+
+	int i, j;
+	va_list a;
+	char *separator;
+
+        fmt_t match[] = {
+		{'c', p_char},
+		{'i', p_integer},
+		{'f', p_float},
+		{'s', p_string},
+		{'\0', NULL}
+
+
+	};
+
+	separator = "";
+	va_start(a, format);
+	i = 0;
+	while (format && format[i])
+
+	{
+
+
+		j = 0;
+		while (match[j].c)
+
+		{
+
+			if (format[i] == (match[j]).c)
+
+			{
+				printf("%s", separator);
+				match[j].f(a);
+				separator = ", ";
+
+			}
+			j++;
+
+		}
+		i++;
+
+	}
+
+	printf("\n";)
+
+		va_and(a);
+}
