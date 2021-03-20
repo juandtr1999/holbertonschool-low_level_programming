fa048e4a5dec08eb8b74ab749a38de55f4ebe416
juandtr1999 <jr9282507@gmail.com>
2021-03-19 22:45:26 -0500

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * tortoise - Arrives before the hare.
+ * Return: None.
+ */
+void __attribute__ ((constructor)) tortoise()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
