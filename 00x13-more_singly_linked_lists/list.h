72296b0ac939fed125252339327a5f871637bb57
juandtr1 <jr9282507@gmail.com>
2021-03-23 15:08:09 -0500

    more_singly_linked_lists

--- 00x13-more_singly_linked_lists/list.h
+++ 00x13-more_singly_linked_lists/list.h
@@ -1,45 +1,31 @@
-# ifndef _LISTS_H
-# definir  _LISTAS_H
-# incluye  < stdlib.h >
-# incluye  < stdio.h >
-/ * *
- * struct listint_s - lista enlazada individualmente
- * @n: entero
- * @next: apunta al siguiente nodo
- *
- * Descripción: estructura de nodo de lista enlazada individualmente
- * para el proyecto Holberton
- * /
-typedef  struct listint_s
+#ifndef LISTS_H
+#define LISTS_H
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
 	int n;
-	struct listint_s * siguiente;
-} listint_t ;
-/ * *
- * struct listint_address - dirección de almacenamiento de clon de lista enlazada individualmente
- * @next: apunta al siguiente nodo
- * @address: dirección de otra lista
- * Descripción: estructura de nodo de lista enlazada individualmente
- * esa dirección de trazo de otro
- * /
-typedef  struct listint_address
-{
-	const  struct listint_s * dirección;
-	struct listint_address * siguiente;
-} listint_addr;
-size_t  print_listint ( const  listint_t * h);
-size_t  listint_len ( const  listint_t * h);
-listint_t * add_nodeint ( listint_t ** encabezado, const  int n);
-listint_t * add_nodeint_end ( listint_t ** encabezado, const  int n);
-void  free_listint ( listint_t * cabeza);
-void  free_listint2 ( listint_t ** cabeza);
-int  pop_listint ( listint_t ** head);
-listint_t * get_nodeint_at_index ( listint_t * encabezado, índice int sin firmar  );
-int  sum_listint ( listint_t * cabeza);
-listint_t * insert_nodeint_at_index ( listint_t ** head, unsigned  int idx, int n);
-int  delete_nodeint_at_index ( listint_t ** head, índice int sin firmar  );
-listint_t * reverse_listint ( listint_t ** encabezado);
-size_t  print_listint_safe ( const  listint_t * head);
-size_t  free_listint_safe ( listint_t ** h);
-listint_t * find_listint_loop ( listint_t * cabeza);
-# endif
+	struct listint_s *next;
+} listint_t;
+int print_listint(const listint_t *h);
+int listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+int print_listint_safe(const listint_t *head);
+int free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif
