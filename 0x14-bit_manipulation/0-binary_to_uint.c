fd310a49f22e65c72a5025f48eff79f4c22605a1
juandtr1 <jr9282507@gmail.com>
2021-03-25 21:49:44 -0500

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,24 @@
-# incluye  " holberton.h "
-/ * *
- * binary_to_uint - Convierte binary a unsigned int
- * @b: número binario
- * Devolución: número convertido.
- * /
-unsigned  int  binary_to_uint ( const  char * b)
+#include "holberton.h"
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: Binary number
+ * Return: Converted number.
+ */
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned  int res;
+	unsigned int res;
 
-	si (! b)
-		return ( 0 );
-	para (i = 0 ; b [i]; i ++)
-		si (b [i]! = ' 1 ' && b [i]! = ' 0 ' )
-			return ( 0 );
-	para (i = 0 ; b [i]; i ++)
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	for (i = 0; b[i]; i++)
 	{
-		res = res << 1 ;
-		si (b [i] == ' 1 ' )
-			res = res + 1 ;
+		res = res << 1;
+		if (b[i] == '1')
+			res = res + 1;
 	}
-	retorno (res);
+	return (res);
+}
